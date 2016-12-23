cb13388e74855378cda72b5a17406945069547bb
Murakami0105 <14ec105@ms.dendai.ac.jp>
2016-12-23 15:51:34 +0900

    Update kadai10.m

--- kadai10.m
+++ kadai10.m
@@ -1,7 +1,7 @@
 clear; % 変数のオールクリア
 
 ORG = imread('Lenna.png'); % 原画像の入力
-ORG = rgb2gray(ORG); %カラーからグレイへの変換
+ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
 imagesc(ORG); colormap('gray'); colorbar;% 画像表示
 title('白黒濃淡画像')
 pause; % 一時停止
