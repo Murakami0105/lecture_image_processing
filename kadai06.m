01325cb9f508709c31e0d4d82888fbed70565a30
Murakami0105 <14ec105@ms.dendai.ac.jp>
2016-12-23 15:48:36 +0900

    Update kadai06.m

--- kadai06.m
+++ kadai06.m
@@ -1,7 +1,7 @@
 clear; % 変数のオールクリア
 
 ORG=imread('Lenna.png'); % 原画像の入力
-ORG = rgb2gray(ORG);
+ORG = rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 title('原画像')
 pause; % 一時停止
