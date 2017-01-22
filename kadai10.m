27b9326d260af2c5be2cfcc6ff3621565c1ac326
Murakami0105 <14ec105@ms.dendai.ac.jp>
2017-01-22 21:35:46 +0900

    Update kadai10.m

--- kadai10.m
+++ kadai10.m
@@ -1,6 +1,6 @@
 clear; % 変数のオールクリア
 
-ORG = imread('Lenna.png'); % 原画像の入力
+ORG = imread('pic1.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
 imagesc(ORG); colormap('gray'); colorbar;% 画像表示
 title('白黒濃淡画像')
